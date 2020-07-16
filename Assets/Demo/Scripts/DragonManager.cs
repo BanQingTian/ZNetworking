219bb66a6f75c35649dbf8ba45b1331838f05e44
Zhuo <zhchen@nreal.ai>
2020-07-16 16:54:03 +0800

    add

---
+++ Assets/Demo/Scripts/DragonManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonManager : MonoBehaviour {
+
+    public ZMain m_Zmain;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
